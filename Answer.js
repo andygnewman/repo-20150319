b545a750ca8619bc62c689c3c2fef87a2ad30c51
Andy <andygnewman@gmail.com>
2015-03-19 13:33:01 +0400

    refactoring - abstracted findIdName and findClassName to findName function

--- Answer.js
+++ Answer.js
@@ -1,61 +1,11 @@
 var $ = function (selector) {
   var elements = [];
 
-// Parse individual selectors
-  var idName;
-  var className;
-  var tagName;
+  var idName = findName(selector, 'id');
+  var className = findName(selector, 'class');
+  var tagName = findTagName(selector);
 
-  var findIdName = function(selector) {
-    if (selector.indexOf('#') !== -1) {
-      var idNameStart = selector.indexOf('#') + 1;
-      var idNameLength;
-      if (selector.substr(selector.indexOf('#')).indexOf('.') !== -1) {
-        idNameLength = selector.substr(selector.indexOf('#') + 1).indexOf('.');
-      }
-      else {
-        idNameLength = selector.length - idNameStart;
-      }
-      idName = selector.substr(idNameStart, idNameLength);
-    }
-  }
-
-  var findClassName = function(selector) {
-    if (selector.indexOf('.') !== -1) {
-      var classNameStart = selector.indexOf('.') + 1;
-      var classNameLength;
-      if (selector.substr(selector.indexOf('.')).indexOf('#') !== -1) {
-        classNameLength = selector.substr(selector.indexOf('.') + 1).indexOf('#');
-      }
-      else {
-        classNameLength = selector.length - classNameStart;
-      }
-      className = selector.substr(classNameStart, classNameLength);
-    }
-  }
-
-  var findTagName = function(selector) {
-    if (selector.slice(0,1).search(/[\W]/) === -1 ) {
-      var tagNameStart = 0;
-      var tagNameLength;
-      if (/[\W]/.exec(selector)) {
-        tagNameLength = selector.indexOf(/[\W]/.exec(selector));
-      }
-      else {
-        tagNameIndexEnd = selector.length;
-      }
-      tagName = selector.substr(tagNameStart, tagNameLength);
-    }
-  }
-
-  findIdName(selector);
-  findClassName(selector);
-  findTagName(selector);
-
-//Find matching elements
-  var resultId = [];
-  var resultClass = [];
-  var resultTag = [];
+  var resultId = [], resultClass = [], resultTag = [];
   var selectorsUsed = 0;
 
   if (idName !== undefined) {
@@ -88,4 +38,34 @@ var $ = function (selector) {
   }
 
   return elements;
+}
+
+var findTagName = function(selector) {
+  if (selector.slice(0,1).search(/[\W]/) === -1 ) {
+    var tagNameStart = 0;
+    var tagNameLength;
+    if (/[\W]/.exec(selector)) {
+      tagNameLength = selector.indexOf(/[\W]/.exec(selector));
+    }
+    else {
+      tagNameIndexEnd = selector.length;
+    }
+    return selector.substr(tagNameStart, tagNameLength);
+  }
+}
+
+var findName = function(selector, type) {
+  if (type === 'class') var first = '.', second = '#';
+  else if (type === 'id') var first = '#', second = '.';
+  else return 'Unhandled type';
+  if (selector.indexOf(first) !== -1) {
+    var nameStart = selector.indexOf(first) + 1;
+    var nameLength;
+    if (selector.substr(selector.indexOf(first)).indexOf(second) !== -1) {
+      nameLength = selector.substr(selector.indexOf(first) + 1).
+        indexOf(second);
+    }
+    else nameLength = selector.length - nameStart;
+    return selector.substr(nameStart, nameLength);
+  }
 }
